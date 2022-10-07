55e72884b84fef492c4704896e6150555dfae04e
github username <Lukabaramishvili@gmail.com>
2022-10-07 12:19:49 +0400

    create userList component to display all users & create individual user dashboard to edit user information

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/pages/home/Home';
 import UserList from './components/pages/userList/UserList';
+import User from './components/pages/user/User';
 import './app.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route path='/users'>
             <UserList />
+          </Route>
+          <Route path='/user/:id'>
+            <User />
           </Route>
         </Switch>
       </div>
---
+++ src/components/pages/user/User.jsx
@@ -0,0 +1,121 @@
+import './user.css';
+import {
+  PermIdentity,
+  CalendarToday,
+  PhoneAndroid,
+  LocationSearching,
+  MailOutline,
+  Publish,
+} from '@material-ui/icons';
+
+export default function User() {
+  return (
+    <div className='user'>
+      <div className='userTitleContainer'>
+        <h1 className='userTitle'>Edit User</h1>
+        <button className='userAddButton'>Create</button>
+      </div>
+      <div className='userContainer'>
+        <div className='userShow'>
+          <div className='userShowTop'>
+            <img
+              src='https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+              alt=''
+              className='userShowImg'
+            />
+            <div className='userShowTopTitle'>
+              <span className='userShowUsername'>Anna Becker</span>
+              <span className='userShowUserTitle'>Software Engineer</span>
+            </div>
+          </div>
+          <div className='userShowBottom'>
+            <spam className='userShowTitle'>Account Details</spam>
+            <div className='userShowInfo'>
+              <PermIdentity className='userShowIcon' />
+              <span className='userShowInfoTitle'>annabeck99</span>
+            </div>
+            <spam className='userShowTitle'>Contact Details</spam>
+            <div className='userShowInfo'>
+              <CalendarToday className='userShowIcon' />
+              <span className='userShowInfoTitle'>10.12.1993</span>
+            </div>
+            <div className='userShowInfo'>
+              <PhoneAndroid className='userShowIcon' />
+              <span className='userShowInfoTitle'>+ 1 123 456 789</span>
+            </div>
+            <div className='userShowInfo'>
+              <MailOutline className='userShowIcon' />
+              <span className='userShowInfoTitle'>annabeck099@gmail.com</span>
+            </div>
+            <div className='userShowInfo'>
+              <LocationSearching className='userShowIcon' />
+              <span className='userShowInfoTitle'>New York, USA</span>
+            </div>
+          </div>
+        </div>
+        <div className='userUpdate'>
+          <span className='userUpdateTitle'>Edit</span>
+          <form className='userUpdateForm'>
+            <div className='userUpdateLeft'>
+              <div className='userUpdateItem'>
+                <label>Username</label>
+                <input
+                  type='text'
+                  className='userUpdateInput'
+                  placeholder='annabeck99'
+                />
+              </div>
+              <div className='userUpdateItem'>
+                <label>Full Name</label>
+                <input
+                  type='text'
+                  className='userUpdateInput'
+                  placeholder='annabeck99'
+                />
+              </div>
+              <div className='userUpdateItem'>
+                <label>Email</label>
+                <input
+                  type='text'
+                  className='userUpdateInput'
+                  placeholder='annabeck099@gmail.com
+                  '
+                />
+              </div>
+              <div className='userUpdateItem'>
+                <label>Phone</label>
+                <input
+                  type='text'
+                  className='userUpdateInput'
+                  placeholder='+ 1 123 456 789'
+                />
+              </div>
+              <div className='userUpdateItem'>
+                <label>Address</label>
+                <input
+                  type='text'
+                  className='userUpdateInput'
+                  placeholder='New York, USA'
+                />
+              </div>
+            </div>
+            <div className='userUpdateRight'>
+              <div className='userUpdateUpload'>
+                <img
+                  src='https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                  alt=''
+                  className='userUpdateImg'
+                />
+                <label htmlFor='file'>
+                  <Publish />
+                </label>
+                <input type='file' id='file' style={{ display: 'none' }} />
+              </div>
+              <button className='userUpdateButton'>Update</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
